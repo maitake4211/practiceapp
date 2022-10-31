cc329a637881e19e13090fb862100e4ccc0228f5
maitake4211 <dmwajtgp4211@gmail.com>
2022-10-31 13:08:08 +0900

    add:新しいテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_094623) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_033951) do
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "password", limit: 12, null: false
